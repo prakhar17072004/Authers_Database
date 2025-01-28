5ff42b02b41197e5807808908d3a6ef2e8558b33
prakhar17072004 <pmankur2004@gmail.com>
2025-01-28 22:33:47 +0530

    new file of drizzle is added

---
+++ src/db/migrations/0000_real_felicia_hardy.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "auther" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"auther_name" varchar(25) NOT NULL,
+	"email" varchar(25) NOT NULL,
+	"bio" text
+);
+--> statement-breakpoint
+CREATE TABLE "books" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"auther_id" integer NOT NULL,
+	"books_name" varchar(25) NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "books" ADD CONSTRAINT "books_auther_id_auther_id_fk" FOREIGN KEY ("auther_id") REFERENCES "public"."auther"("id") ON DELETE no action ON UPDATE no action;
