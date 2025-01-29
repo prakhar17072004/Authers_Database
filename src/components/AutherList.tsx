c7ab4e968f22ebd455b3e96b6a043f0a03fc7229
prakhar17072004 <pmankur2004@gmail.com>
2025-01-29 20:23:42 +0530

    cards are updated

---
+++ src/components/AutherList.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+
+interface AUTHER {
+  id: number;
+  auther_name: string;
+  email: string;
+  bio: string;
+}
+interface BOOKS {
+  id: number;
+  auther_id:number;
+  books_name: string;
+  
+}
+
+const AutherList: React.FC = () => {
+  const [authers, setAuthers] = useState<AUTHER[]>([]);
+  const [books, setBooks] = useState<BOOKS[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchAuthers = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/authers');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      
+      setAuthers(data);
+      
+    } catch (error: any) {
+      setError(error.message || 'An error occurred');
+      console.error('Error fetching authers:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/books');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      
+      setBooks(data);
+      
+    } catch (error: any) {
+      setError(error.message || 'An error occurred');
+      console.error('Error fetching authers:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    
+fetchAuthers();
+fetchBooks();
+    
+  }, []);
+
+  console.log(authers);
+  console.log(books);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+  <h1 className="text-center bg-slate-900 text-white">Author List</h1>
+  <ul className="grid grid-cols-3 gap-4 p-4">
+    {authers.map((author) => (
+      <li
+        key={author.id}
+        className="border rounded-lg p-4 bg-white shadow-md flex flex-col items-start"
+      >
+        <span className="font-semibold text-gray-800">Authors Name:{author.auther_name}</span>
+        <ul>
+          {books.filter((books)=>books.auther_id===author.id).map((books)=>(
+            <li key={books.id}>
+              
+              <span className="text-gray-600 flex">Books Written:{books.books_name}</span>
+
+            </li>
+          ))}
+        </ul>
+        
+        <span className="text-gray-600"> Authors Eamil:{author.email}</span>
+        <span className=" text-slate-950"> Auther Bio:{author.bio}</span>
+      </li>
+    ))}
+  </ul>
+</div>
+
+  );
+};
+
+export default AutherList;
---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "../app/globals.css";
+import Navbar from '@/components/Navbar';
+import AutherList from '@/components/AutherList';
+import Dashboard from '@/components/Dashboard';
+import Link from 'next/link';
+
+const Index: React.FC = () => {
+  return(
+    
+        <div >
+       
+       <div className='bg-slate-600 text-center'>
+       <Navbar/>
+       
+       </div>
+       <Link href="/home">
+       <AutherList/>
+       {/* <Dashboard/> */}
+       </Link>
+       
+       
+       
+        </div>
+    
+  )
+};
+
+export default Index;
