59d303adf3edc28700d994f8c14badbe938b23df
prakhar17072004 <pmankur2004@gmail.com>
2025-01-30 22:46:51 +0530

    new books page is added

---
+++ src/pages/api/books.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BooksPage() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-2xl font-bold">Books List</h1>
+      <p>Here, you will find a list of all books.</p>
+    </div>
+  );
+}
