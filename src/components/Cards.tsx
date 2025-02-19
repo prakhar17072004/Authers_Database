c89624b326acba6af4179f1d017cb2f4b7f1c267
prakhar17072004 <pmankur2004@gmail.com>
2025-02-19 23:10:14 +0530

    cards  is updated

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -6,8 +6,8 @@ function Cards() {
   return (
     <div className='mt-8'>
         <div className='grid grid-cols-3 max-w-[1250px] ml-[190px] gap-4 rounded-md text-center'>
-            <div className='bg-slate-400 rounded-3xl'>
-            <div className='m-12 bg-cyan-800 rounded-full text-xl'>
+            <div className='bg-slate-500 rounded-3xl'>
+            <div className='m-12 bg-cyan-900 rounded-full text-xl'>
             <Image src={pic} alt ="pic" className=" p-4 mx-auto"/>
             </div>
             <h2>Auther Name:<span>Jai shanker prashad</span></h2>
