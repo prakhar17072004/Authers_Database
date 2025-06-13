d057727d5db3f248184f23c987fa3dbe3444e7b4
Prakhar Mishra <94304386+prakhar17072004@users.noreply.github.com>
2025-06-13 22:28:52 +0530

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,7 +3,7 @@ import Cards from './Cards'
 
 function Dashboard() {
   return (
-    <div className='bg-gray-700 h-[100vh]'>
+    <div className='bg-gray-800 h-[100vh]'>
         <h1 className='font-bold text-center'>AUTHER DETAILS </h1>
         <Cards/>
     </div>
